ac8523ca25ca50357e7048bd86b180887014daef
myfly2013 <myfly2013@yeah.net>
2019-07-26 22:24:03 +0800

    feat: add display

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
     <form-demo />
+    <i-display :code="code"></i-display>
   </div>
 </template>
 
 <script>
 import formDemo from './form_demo'
+import iDisplay from './components/display/display.vue'
+import defaultCode from './default-code.js'
 
 export default {
   name: 'app',
   components: {
-    formDemo
+    formDemo,
+    iDisplay
+  },
+  data () {
+    return {
+      code: defaultCode
+    }
   }
 }
 </script>
